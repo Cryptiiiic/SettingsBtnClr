6ae2c3620b03766d3b59f1da3400d86b3a7d14ee
Maddox Norman <38148834+RustyBalboadev@users.noreply.github.com>
2018-06-26 13:44:29 -0500

    Added support for libcolorpicker

--- Tweak.mm
+++ Tweak.mm
@@ -1,17 +1,23 @@
 #import <version.h>
-
+#import "libcolorpicker.h"
+#define PLIST_PATH @"/var/mobile/Library/Preferences/com.cryptic.prefsbuttoncolor.plist"
+#define prefsDict [NSDictionary dictionaryWithContentsOfFile:PLIST_PATH]
+static NSString *TextColor;
+static NSString *ArrowColor;
+static NSString *tenTextColor;
+static NSString *tenArrowColor;
 %group iOS11
 %hook UINavigationBar
 -(UIColor *)tintColor
 {
-	UIColor *color = [UIColor redColor];
+	UIColor *color = LCPParseColorString([prefsDict objectForKey:@"TextColor"], @"#007AFF");
 	return color;
 }
 %end
 %hook UINavigationBarBackIndicatorView
 -(UIColor *)tintColor
 {
-        UIColor *color = [UIColor redColor];
+        UIColor *color = LCPParseColorString([prefsDict objectForKey:@"ArrowColor"], @"#007AFF");
         return color;
 }
 %end
@@ -21,15 +27,15 @@
 %hook UINavigationBar
 -(UIColor *)tintColor
 {
-	UIColor *color = [UIColor redColor];
+	UIColor *color = LCPParseColorString([prefsDict objectForKey:@"tenTextColor"], @"007AFF");
 	return color;
 }
 %end
 %hook _UINavigationBarBackIndicatorView
 -(UIColor *)tintColor
 {
-        UIColor *color = [UIColor redColor];
-        return color;
+	UIColor *color = LCPParseColorString([prefsDict objectForKey@"tenArrowColor"], @"007AFF");
+	return color;
 }
 %end
 %end
