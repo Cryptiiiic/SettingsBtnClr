2ade458a718dedc7b2841b8d688f39fce2c7a2f5
Cryptiiiic <liamwqs@gmail.com>
2018-06-26 22:00:51 -0700

    Minor fixes and improvements, replaced system with posix_spawn

--- Tweak.mm
+++ Tweak.mm
@@ -1,11 +1,6 @@
 #import <version.h>
 #import "libcolorpicker.h"
-#define PLIST_PATH @"/var/mobile/Library/Preferences/com.cryptic.prefsbuttoncolor.plist"
-#define prefsDict [NSDictionary dictionaryWithContentsOfFile:PLIST_PATH]
-static NSString *TextColor;
-static NSString *ArrowColor;
-static NSString *tenTextColor;
-static NSString *tenArrowColor;
+NSDictionary *prefsDict = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.cryptic.prefsbuttoncolor.plist"];
 %group iOS11
 %hook UINavigationBar
 -(UIColor *)tintColor
@@ -27,14 +22,14 @@ static NSString *tenArrowColor;
 %hook UINavigationBar
 -(UIColor *)tintColor
 {
-	UIColor *color = LCPParseColorString([prefsDict objectForKey:@"tenTextColor"], @"007AFF");
+	UIColor *color = LCPParseColorString([prefsDict objectForKey:@"TextColor"], @"#007AFF");
 	return color;
 }
 %end
 %hook _UINavigationBarBackIndicatorView
 -(UIColor *)tintColor
 {
-	UIColor *color = LCPParseColorString([prefsDict objectForKey@"tenArrowColor"], @"007AFF");
+	UIColor *color = LCPParseColorString([prefsDict objectForKey:@"ArrowColor"], @"#007AFF");
 	return color;
 }
 %end
